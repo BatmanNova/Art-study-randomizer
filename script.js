337db0c163acc5a67ddb1dce3182eeb6d27370d1
batmannova <dogmancow527@gmail.com>
2023-04-16 13:39:47 -0400

    removed comment block

--- script.js
+++ script.js
@@ -8,22 +8,6 @@ const animalRegex = /Fish|Birds|Mammals|Insects|Reptiles/;
 const miscCategoryRegex = /Environments|Fashion|Architecture|Film|Portraits/;
 const twoWordCategoryRegex = /Still_life/;
 
-//The flow of the program is as follows:
-//1. User selects categories of art studies they want to study
-//2. User clicks the spin button
-//3. The spin button calls the spinWheel function
-//4. The spinWheel function calls the randomizeListContents function
-//5. The randomizeListContents function calls the getListContents function
-//6. The getListContents function calls the getSelectedLists function
-//7. The getSelectedLists function checks if any checkboxes are checked, if none, "check" all
-//8. The getSelectedLists function returns an array of the selected checkboxes
-//9. The getListContents function iterates through the array of selected checkboxes, pushing contents of all to 1 array
-//10. The randomizeListContents function randomizes the array of all contents of selected categories
-//11. The spinWheel function displays a random element from the randomized array
-//12. Once the spinWheel duration ends, the spinWheel function displays the category and subject of the random element (winner)
-//13. When the Browse images button is pressed, the categoryBox is checked against the regex to clean or search
-//14. SubjectBox contents are used to search for images dependant upon radio button selection
- 
 function getSelectedLists() {
     var selectedCategories = [];
     var checkedBoxes = document.querySelectorAll('input[type=checkbox]:checked');
